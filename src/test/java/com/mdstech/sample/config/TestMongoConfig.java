56d90b139b3e7894b333f5261bc425385568eee5
Srini Maddineni <srini@mdstechinc.com>
2019-02-12 21:22:12 -0500

    Fixed unit test cases using in memory mongo java server

--- src/test/java/com/mdstech/sample/config/TestMongoConfig.java
+++ src/test/java/com/mdstech/sample/config/TestMongoConfig.java
@@ -1,7 +1,10 @@
 package com.mdstech.sample.config;
 
-import com.github.fakemongo.Fongo;
+//import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -19,24 +22,47 @@ import java.util.Collections;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.mdstech.sample.repository")
-@ConditionalOnProperty(name = "inmemory.monogodb.datatbase")
-public class TestMongoConfig extends AbstractMongoConfiguration {
+//@ConditionalOnProperty(name = "inmemory.monogodb.datatbase")
+@ComponentScan(basePackages = {"com.mdstech.sample.service", "com.mdstech.sample.events", "com.mdstech.sample.domain"})
+public class TestMongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoDbFactory(mongoClient));
+    }
+
+    @Bean
+    public MongoDbFactory mongoDbFactory(MongoClient mongoClient) {
+        return new SimpleMongoDbFactory(mongoClient, "test");
+    }
+
+    @Bean(destroyMethod="shutdown")
+    public MongoServer mongoServer() {
+        MongoServer mongoServer = new MongoServer(new MemoryBackend());
+        mongoServer.bind();
+        return mongoServer;
+    }
+
+    @Bean(destroyMethod="close")
+    public MongoClient mongoClient(MongoServer mongoServer) {
+        return new MongoClient(new ServerAddress(mongoServer.getLocalAddress()));
+    }
 
     @Value("${inmemory.monogodb.datatbase}")
     private String database;
 
-    @Override
-    protected Collection<String> getMappingBasePackages() {
-        return Collections.singletonList("com.mdstech.sample.domain");
-    }
-
-    @Override
-    public MongoClient mongoClient() {
-        return new Fongo(getDatabaseName()).getMongo();
-    }
-
-    @Override
-    protected String getDatabaseName() {
-        return database;
-    }
+//    @Override
+//    protected Collection<String> getMappingBasePackages() {
+//        return Collections.singletonList("com.mdstech.sample.domain");
+//    }
+//
+//    @Override
+//    public MongoClient mongoClient() {
+//        return new Fongo(getDatabaseName()).getMongo();
+//    }
+//
+//    @Override
+//    protected String getDatabaseName() {
+//        return database;
+//    }
 }
---
+++ src/test/java/com/mdstech/sample/service/CustomerServiceTest.java
@@ -0,0 +1,22 @@
+package com.mdstech.sample.service;
+
+import com.mdstech.sample.SpringUnitTestCaseHelper;
+import com.mdstech.sample.domain.Customer;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class CustomerServiceTest extends SpringUnitTestCaseHelper {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Test
+    public void testCRUD() {
+        Customer customer1 = customerService.save(new Customer("test", "test1"));
+        assertThat(customer1.getId(), is(notNullValue()));
+    }
+
+}
