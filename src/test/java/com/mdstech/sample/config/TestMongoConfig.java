c1adebc1b772ea515706bee7eed9de3b92fcd1e0
Srini Maddineni <srini@mdstechinc.com>
2019-02-12 21:32:06 -0500

    Implemented integration tests using in memory mongo server

--- src/test/java/com/mdstech/sample/config/TestMongoConfig.java
+++ src/test/java/com/mdstech/sample/config/TestMongoConfig.java
@@ -1,29 +1,20 @@
 package com.mdstech.sample.config;
 
-//import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import de.bwaldvogel.mongo.MongoServer;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.Collection;
-import java.util.Collections;
-
 @Configuration
 @EnableMongoRepositories(basePackages = "com.mdstech.sample.repository")
-//@ConditionalOnProperty(name = "inmemory.monogodb.datatbase")
-@ComponentScan(basePackages = {"com.mdstech.sample.service", "com.mdstech.sample.events", "com.mdstech.sample.domain"})
 public class TestMongoConfig {
 
     @Bean
@@ -35,7 +26,7 @@ public class TestMongoConfig {
     @Bean
     @Primary
     public MongoDbFactory mongoDbFactory(MongoClient mongoClient) {
-        return new SimpleMongoDbFactory(mongoClient, "test");
+        return new SimpleMongoDbFactory(mongoClient, database);
     }
 
     @Bean(destroyMethod="shutdown")
@@ -54,18 +45,4 @@ public class TestMongoConfig {
     @Value("${inmemory.monogodb.datatbase}")
     private String database;
 
-//    @Override
-//    protected Collection<String> getMappingBasePackages() {
-//        return Collections.singletonList("com.mdstech.sample.domain");
-//    }
-//
-//    @Override
-//    public MongoClient mongoClient() {
-//        return new Fongo(getDatabaseName()).getMongo();
-//    }
-//
-//    @Override
-//    protected String getDatabaseName() {
-//        return database;
-//    }
 }
